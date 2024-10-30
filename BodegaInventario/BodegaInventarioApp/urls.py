a34ec27ca7a2f09e23453666bae4291b9eed61cf
laualca <lmalvarez1@uth.hn>
2024-10-29 21:20:10 -0600

    noche 29 commit inventario app

---
+++ BodegaInventario/BodegaInventarioApp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from BodegaInventarioApp import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+#Rutas de la aplicacion
+urlpatterns = [
+    
+    path('', views.inicio, name='Inicio'),
+    path('mantenimiento/', views.mantenimiento, name='Mantenimiento'),
+    path('transacciones/', views.transacciones, name='Transacciones'),
+    path('reportes/', views.reportes, name='Reportes'),
+    
+]
+#para poder visualizar las imagenes en el navegador
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ BodegaInventario/BodegaInventarioApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'BodegaInventarioApp/inicio.html')
+
+def mantenimiento(request):
+    return render(request, 'BodegaInventarioApp/mantenimiento.html')
+
+def transacciones(request):
+    return render(request, 'BodegaInventarioApp/transacciones.html')
+
+
+
+def reportes(request):
+    return render(request, 'BodegaInventarioApp/reportes.html')
